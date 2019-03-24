13b3fa47f0a96c933a67cece8a3c419222d8e922
Joe Wingard <joseph.wingard@jefferson.edu>
2019-03-24 07:29:43 -0400

    wip

--- src/queries.js
+++ src/queries.js
@@ -1,10 +1,49 @@
 const net = require('./network-resources.js')
 const db = net.db
 
-function readProviders() {
+function countProviders() {
+  return db.from('cms.providers').count()
+}
+module.exports.countProviders = countProviders
+
+function readProviders(lmt = 10) {
   return db.from('cms.providers')
     .select(['npi', 'entity_type', 'address_latitude', 'address_longitude'])
-    .limit(10)
+    .limit(lmt)
+}
+module.exports.readProviders = readProviders
+
+const performanceFields = [
+  'npi',
+  'hcpcs_code',
+  'n_of_svcs',
+  'n_of_distinct_mcare_beneficiary_per_day_svcs',
+  'n_of_mcare_beneficiaries',
+  'avg_mcare_pay_amt',
+  'avg_submitted_charge_amt',
+  'avg_mcare_allowed_amt',
+  'avg_mcare_standardized_amt',
+  'est_ttl_mcare_pay_amt',
+  'est_ttl_submitted_charge_amt',
+  'rank_n_of_svcs',
+  'rank_n_of_distinct_mcare_beneficiary_per_day_svcs',
+  'rank_n_of_mcare_beneficiaries',
+  'rank_avg_mcare_standardized_amt',
+  'rank_avg_mcare_allowed_amt',
+  'rank_avg_submitted_charge_amt',
+  'rank_avg_mcare_pay_amt',
+  'rank_est_ttl_mcare_pay_amt',
+  'rank_est_ttl_submitted_charge_amt',
+  'mcare_participation_indicator',
+  'place_of_service',
+  'var_avg_mcare_submitted_charge_pay_amt',
+  'rank_var_avg_mcare_submitted_charge_pay_amt'
+]
+
+function readProviderPerformance(npis) {
+  return db.from('cms.service_provider_performance')
+    .select(performanceFields)
+    .whereIn('npi', npis)
 }
 
-module.exports.readProviders = readProviders
+module.exports.readProviderPerformance = readProviderPerformance
--- src/test.js
+++ src/test.js
@@ -1,8 +1,35 @@
 const data = require('./queries.js')
 
-const records = data.readProviders()
-    .then(records => {
-        console.log(records)
+
+const run = async () => {
+    const countResponse = await data.countProviders()
+    const count = parseInt(countResponse[0].count, 10)
+    console.log(count)
+
+    const providers = await data.readProviders().map(provider => {
+        return {
+            npi: provider.npi,
+            entity_type: provider.entity_type,
+            location: {
+                lat: provider.address_latitude,
+                lon: provider.address_latitude,
+            }
+        }
     })
 
+    const batchPerformances = {}
+    for (let provider of providers) {
+        batchPerformances[provider.npi] = []
+    }
+    const providerPerformances = await data.readProviderPerformance(providers.map(provider => provider.npi))
+    for (let performance of providerPerformances) {
+        batchPerformances[performance.npi].push(performance)
+    }
+    
+    console.log(batchPerformances)
 
+    process.exit(0)
+
+}
+
+run()
