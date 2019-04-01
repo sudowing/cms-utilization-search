ea10bfb9e772f79a5234e46d5b943d1d3d4c3435
Joe Wingard <joseph.wingard@jefferson.edu>
2019-03-31 22:42:06 -0400

    wip

--- README.md
+++ README.md
@@ -443,3 +443,12 @@ import
 
 
 
+	docker run \
+	--rm \
+    --net=host \
+	-v ${PWD}/volumes/elastic_exports:/tmp/elastic_exports \
+    -e ES_SERVICE="http://localhost:9200" \
+	--entrypoint bash \
+	--name cms-elasticsearch-exporter \
+	sudowing/cms-utilization-search:develop \
+	-c 'bash /scripts/seed.index.sh'
---
+++ scripts/seed.index.sh
@@ -0,0 +1,115 @@
+echo "UNCOMPRESSING ELASTIC INDEX EXPORTS [start]" \
+    &&
+echo "" \
+    &&
+# tar -xvf /elastic_exports.tar.bz2 -C /tmp \
+#     &&
+echo "UNCOMPRESSING ELASTIC INDEX EXPORTS [start]" \
+    &&
+echo "" \
+    &&
+echo "" \
+    &&
+echo "***********************" \
+    &&
+echo "SEEDING ELASTIC [start]" \
+    &&
+echo "***********************" \
+    &&
+echo "" \
+    &&
+echo "LOADING SERVICE INDEX [start]" \
+    &&
+echo "" \
+    &&
+echo "loading service analyzer" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/services.analyzer.json \
+    --output=${ES_SERVICE}/services \
+    --type=analyzer \
+    && \
+echo "loading service mapping" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/services.mapping.json \
+    --output=${ES_SERVICE}/services \
+    --type=mapping \
+    && \
+echo "loading service data" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/services.data.json \
+    --output=${ES_SERVICE}/services \
+    --type=data \
+    && \
+echo "" \
+    &&
+echo "LOADING SERVICE INDEX [complete]" \
+    &&
+echo "" \
+echo "LOADING PROVIDERS INDEX [start]" \
+    &&
+echo "" \
+    &&
+echo "loading providers analyzer" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/providers.analyzer.json \
+    --output=${ES_SERVICE}/providers \
+    --type=analyzer \
+    && \
+echo "loading providers mapping" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/providers.mapping.json \
+    --output=${ES_SERVICE}/providers \
+    --type=mapping \
+    && \
+echo "loading providers data" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/providers.data.json \
+    --output=${ES_SERVICE}/providers \
+    --type=data \
+    && \
+echo "" \
+    &&
+echo "LOADING PROVIDERS INDEX [complete]" \
+    &&
+echo "" \
+echo "LOADING PROVIDER-PERFORMANCE INDEX [start]" \
+    &&
+echo "" \
+    &&
+echo "loading provider-performance analyzer" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/provider-performance.analyzer.json \
+    --output=${ES_SERVICE}/provider-performance \
+    --type=analyzer \
+    && \
+echo "loading provider-performance mapping" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/provider-performance.mapping.json \
+    --output=${ES_SERVICE}/provider-performance \
+    --type=mapping \
+    && \
+echo "loading provider-performance data" \
+    && \
+elasticdump \
+    --input=/tmp/elastic_exports/provider-performance.data.json \
+    --output=${ES_SERVICE}/provider-performance \
+    --type=data \
+echo "" \
+    &&
+echo "LOADING PROVIDER-PERFORMANCE INDEX [complete]" \
+    &&
+echo "" \
+    &&
+echo "***********************" \
+    &&
+echo "SEEDING ELASTIC [complete]" \
+    &&
+echo "***********************"
